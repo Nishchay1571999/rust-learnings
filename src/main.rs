8c1c55d267fc6dca47ddbd306d3f9dc5714b45d1
Nishchay Bhatt <nishchay.bhat@gmail.com>
2024-05-12 23:19:07 +0530

    Itertive Struct Display in the fmt Display Implementations

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::fmt::{self};
+use std::fmt::{self, write};
 
 
 
@@ -12,6 +12,28 @@ struct ComplexNumbers {
     x:i32,
     y:i32,
 }
+// Struct with aList in it
+#[derive(Debug)]
+struct List(Vec<i32>);
+
+
+impl fmt::Display for List {
+    fn fmt(&self, f:&mut fmt::Formatter) -> fmt::Result {
+        // Extract the value using Tueple Indexing and create a refference to the vector
+        let vec = &self.0;
+        write!(f, "[")?;
+        // Iterating over the Vector while enumerating the iteration
+
+        // No clue what enumerating the Iteration means. Doesnt Matter for now
+
+        for (count,v) in vec.iter().enumerate() {
+            if count != 0 { write!(f, ", ")?; }
+            write!(f, "{}:{1}",count,v)?;
+        }
+
+        write!(f, "]")
+    }
+}
 
 impl fmt::Display for ComplexNumbers {
     fn fmt(&self, f:&mut fmt::Formatter) -> fmt::Result {
@@ -24,15 +46,20 @@ impl fmt::Display for Person {
         write!(f, "My name is {1} and my age is {0}",self.age,self.name)
     }
 }
+
+
+
+
 fn main() {
     let person = Person{
         name:String::from("Nishchay"),
         age:24
     };
-    println!("{}",person);
     let complex = ComplexNumbers { 
         x:10,
         y:20
     };
-    println!("{}",complex);
+
+    let ve  =  List(vec![1,2,3,4,5,6,7]);
+    println!("{}",ve)
 }
